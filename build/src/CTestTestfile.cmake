5311f5050474805725ea599f4ecc1dcabf1fe1d8
vbalasubrama <Vasudev Balasubramanian>
2015-12-13 13:54:04 -0500

    Added gitignore

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/rapmap_compbio/src
-# Build directory: /usr/rapmap_compbio/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
